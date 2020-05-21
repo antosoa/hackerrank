be0731ffaef90f880e35aba0a2c278bba782daed
Antonio Soardi <40772919+antosoa@users.noreply.github.com>
2020-05-21 11:14:08 +0200

    add solution

---
+++ NotAlone.java
@@ -0,0 +1,18 @@
+public class NotAlone {
+	
+	public int[] notAlone(int[] nums, int val) {
+		  
+		  for(int i=1; i<nums.length-1; i++)
+		  {
+		    if(nums[i]==val && nums[i-1]!=val && nums[i+1]!=val)
+		    {
+		      //int max=nums[i-1]<nums[i+1]?nums[i+1]:nums[i-1];
+		      nums[i]=nums[i-1]<nums[i+1]?nums[i+1]:nums[i-1];
+		    }
+		  }
+		  return nums;
+		}
+
+	
+
+}
