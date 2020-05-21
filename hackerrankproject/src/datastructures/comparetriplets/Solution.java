5b6e23ea7d33f844fcc8323131de281dc796e3be
antonio soardi <antonio.soardi9@gmail.com>
2020-05-21 14:23:34 +0200

    add solution

---
+++ hackerrankproject/src/datastructures/comparetriplets/Solution.java
@@ -0,0 +1,75 @@
+package datastructures.comparetriplets;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        
+        Integer countA=0;
+        Integer countB=0;
+        List<Integer> res = new ArrayList<Integer>();
+        
+            for(int i=0, j=0; i<a.size(); i++, j++)
+            {
+             if(a.get(i)> b.get(j))
+              countA++;
+             else if(a.get(i) == b.get(j))
+             continue;
+             else
+             countB++;
+  
+            }
+            
+            res.add(countA);
+            res.add(countB);
+        
+            return res;
+        
+     }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] aItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a.add(aItem);
+        }
+
+        String[] bItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bItems[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ hackerrankproject/src/datastructures/dynamicarray/Solution.java
@@ -0,0 +1,10 @@
+package datastructures.dynamicarray;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
