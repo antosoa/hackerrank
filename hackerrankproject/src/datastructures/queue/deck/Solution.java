7dbf0103dcd2836a8a103f461e4fe4c873179394
antonio soardi <antonio.soardi9@gmail.com>
2020-07-11 17:17:31 +0200

    add

---
+++ hackerrankproject/src/datastructures/queue/deck/Solution.java
@@ -0,0 +1,44 @@
+package datastructures.queue.deck;
+
+import java.util.*;
+  
+    public class Solution {
+
+      public static int numberOfUniqueIntegers(ArrayDeque<Integer> deck,int n, int m)
+   {
+   
+       ArrayList<Integer> list = new ArrayList<>();
+
+    for(int i = 0, j= m; i<=(n-m+1); i++, j--)
+    {    
+      ArrayDeque<Integer> clone = deck.clone();
+      for(int k=j; k>=0; k--)
+      {
+          clone.removeLast();
+      } 
+      for(int z=1; z<i; z++)
+      {
+          clone.removeFirst();
+      } 
+      list.add((int) clone.stream().distinct().count());
+     }        
+    return  Collections.max(list);
+   }
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
+            int n = in.nextInt();
+            int m = in.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                int num = in.nextInt();
+                deque.add(num);
+            }
+            int res = numberOfUniqueIntegers(deque,n, m);
+            System.out.println(res);
+        }
+    }
+
+
+
